da56474e0e70787ec9f9bc102f3ad01fb64eb64c
Matt Halverson <mhalverson@onekingslane.com>
2014-05-08 11:13:56 -0700

    Moar examples

--- src/roxxi/utils/matt.clj
+++ src/roxxi/utils/matt.clj
@@ -2,9 +2,24 @@
   (* 2 number))
 
 (defn double-a-list [list-of-numbers]
-  (map double list-of-numbers)
+  (map double list-of-numbers))
 
 (double-a-list [1 2 24 50]) ;; => [2 4 48 100]
+
+(defn transform-a-list [your-function list-of-numbers]
+  (map your-function list-of-numbers))
+
+
+(defn special-fun [my-fun your-fun & things]
+  (my-fun (first things) (your-fun (rest things))))
+
+(special-fun + average [1 2 3 4])
+ ;; => (+ (first [1 2 3 4]) (average (rest [1 2 3 4])))
+ ;; => (+ 1 (average (rest [1 2 3 4])))
+ ;; => (+ 1 (average [2 3 4]))
+ ;; => (+ 1 3)
+ ;; => 4
+
 
 (defn project-map [some-map & {:keys [key-xform value-xform]}]
   (let [
